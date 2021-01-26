10478a98c313ec65365daabc1d8f00b2a817f504
Vrajesh Patel <vrajeshpatel4u@gmail.com>
2021-01-26 11:50:16 +0530

    Magento2 Use ViewModel In list.phtml Instead Of ObjectManager

---
+++ app/code/V4U/ViewModel/ViewModel/CategoryInfo.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created By : Rohan Hapani
+ */
+namespace V4U\ViewModel\ViewModel;
+
+class CategoryInfo implements \Magento\Framework\View\Element\Block\ArgumentInterface
+{
+        protected $_registry;
+
+         /**
+          * @param \Magento\Framework\Registry $registry,
+         */
+
+            public function __construct(
+                \Magento\Framework\Registry $registry
+            ) {
+                $this->_registry = $registry;
+            }
+
+    /**
+     * Return catalog current category object
+     * @return \Magento\Catalog\Model\Category
+     */
+
+    public function getCurrentCategoryOb()
+    {
+        return $this->_registry->registry('current_category');
+    }  
+}
